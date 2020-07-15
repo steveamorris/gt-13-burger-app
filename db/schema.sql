e7101251f5953d073212a866cb428e7fcf71ae3a
theendisfar <r3steve@gmail.com>
2020-07-14 21:25:13 -0400

    let connection not const connection

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN DEFAULT 0,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY(id)
 );
